568e6619a99cce446061a8c6937de54aafb2ab28
Tyler Conn <tyler.conn@hotmail.com>
2018-07-26 01:46:56 -0400

    Show just hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
-  
+  hero: Hero = {
+    id:1,
+    name: 'Tyler'
+  };
+
   constructor() { }
 
   ngOnInit() {
