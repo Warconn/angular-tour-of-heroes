d0195bbbdb728c208126660773d9522737b33578
Tyler Conn <tyler.conn@hotmail.com>
2018-07-26 20:18:07 -0400

    Routed HeroDetails

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,24 +8,18 @@ import { MessageService  } from "../message.service";
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-  selectedHero: Hero;
 
-  constructor(private heroService: HeroService, public messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroService:' + hero.name +' fetched hero');
-  }
-  
   ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
